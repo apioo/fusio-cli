8cfb0647f25f8518525245b3008a6100d9a43a96
Christoph Kappestein <christoph.kappestein@gmail.com>
2021-01-04 20:59:15 +0100

    add script to build phar

---
+++ build/build_phar.php
@@ -0,0 +1,35 @@
+<?php
+
+$stub = <<<TEXT
+<?php
+/*
+ * The Fusio CLI client
+ *
+ * Copyright (C) 2015-2020 Christoph Kappestein <christoph.kappestein@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Phar::mapPhar('fusio.phar');
+
+require 'phar://fusio.phar/bin/fusio';
+
+__HALT_COMPILER();
+
+TEXT;
+
+$phar = new Phar('fusio.phar', 0, 'fusio.phar');
+$phar->buildFromDirectory(__DIR__ . '/../');
+$phar->setStub($stub);
+
